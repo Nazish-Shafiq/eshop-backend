bfb1aa55a8a83f2928829d1889c40786d1586e1c
nazishshafiq <nazishshafiq133@gmail.com>
2026-05-14 21:22:22 +0500

    fix users migration promise

--- migrations/20250420114058_create_users_table.js
+++
@@ -1,9 +0,0 @@
-// Example: create_users_table.js
-exports.up = function(knex) {
-    // already created in DB, so leave empty
-  };
-  
-  exports.down = function(knex) {
-    // optional: could DROP TABLE if you want, or leave empty
-  };
-  
--- migrations/20250420120102_create_users_table.js
+++ migrations/20250420120102_create_users_table.js
@@ -1,14 +1,15 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('users', function(table) {
-    table.increments('user_id').primary();
-    table.boolean('is_admin').defaultTo(false);
-    table.string('user_name', 255).notNullable();
-    table.string('email', 255).notNullable();
-    table.string('password', 255).notNullable();
-    table.string('phone', 20);
-    table.string('city', 255);
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.increments("user_id").primary();
+    table.boolean("is_admin").defaultTo(false);
+    table.string("user_name", 255).notNullable();
+    table.string("email", 255).notNullable();
+    table.string("password", 255).notNullable();
+    table.string("phone", 20);
+    table.string("city", 255);
   });
 };
-exports.down = function(knex) {
-  return knex.schema.dropTable('users');
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
 };
