ffe9b7918585d90ef59b3bc1cd876f16fe6dade9
nazishshafiq <nazishshafiq133@gmail.com>
2026-05-14 21:11:55 +0500

    fix migration order and promises

--- migrations/20250420114218_create_products_table.js
+++ migrations/20250420114218_create_products_table.js
@@ -1,14 +1,15 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('products', function(table) {
-    table.increments('product_id').primary();
-    table.string('product_name', 255).notNullable();
-    table.string('brand', 255);
-    table.decimal('price', 10, 2).notNullable();
-    table.text('description');
-    table.boolean('is_featured').defaultTo(false);
-    table.integer('category_id');
+exports.up = function (knex) {
+  return knex.schema.createTable("products", function (table) {
+    table.increments("product_id").primary();
+    table.string("product_name", 255).notNullable();
+    table.string("brand", 255);
+    table.decimal("price", 10, 2).notNullable();
+    table.text("description");
+    table.boolean("is_featured").defaultTo(false);
+    table.integer("category_id");
   });
 };
-exports.down = function(knex) {
-  return knex.schema.dropTable('products');
+exports.down = function (knex) {
+  return knex.schema.dropTable("products");
 };
+s
