ad181d0cea7e58f41987058f6abc07eea710aec8
nazishshafiq <nazishshafiq133@gmail.com>
2026-05-14 18:01:49 +0500

    fix knexfile production config

--- knexfile.js
+++ knexfile.js
@@ -32,14 +32,14 @@ module.exports = {
     },
   },
 
-  production: {
+ production: {
     client: "pg",
     connection: {
-      host: "localhost", // Should be the hostname for your production environment
-      port: 5432,
-      user: "postgres",
-      password: "12345",
-      database: "eshop_db",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
     },
     pool: {
       min: 2,
